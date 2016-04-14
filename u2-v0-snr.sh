f2681b023b9ae582c84d38aa7ffa53a6a47fc201
Marion Foare <marion.foare@univ-savoie.fr>
2016-04-14 19:04:57 +0200

    scripts

--- u2-v0-snr.sh
+++ u2-v0-snr.sh
@@ -3,172 +3,68 @@
 LANG=C
 
 PROG="u2-v0"
-FILE='res/'$PROG
-EXEC='at-'$PROG
+FILE='snr/'$PROG
+EXEC='at-'$PROG'-snr'
 
 (cd build && make $EXEC)
 
-
-## carre et losange
-
-#E1=1.0
-#E2=0.25
-
-#L1=0.05
-#L2=0.001
-#LR=1.2
-
-#T=64
-
-#BRUIT=("0" "2" "4" "8")
-#ALPHA_B00="0.2 		0.5		0.75"
-#ALPHA_B02="0.05 	0.1 	0.25"
-#ALPHA_B04="0.025 	0.05	0.12"
-#ALPHA_B08="0.012	0.25	0.06"
-#ALPHA_B=("$ALPHA_B00" "$ALPHA_B02" "$ALPHA_B04" "$ALPHA_B08")
-
-#for I in $(seq 0 1 3)
-#do
-#	B=${BRUIT[$I]}
-#	ALPHA=${ALPHA_B[$I]}
-#	for A in $ALPHA
-#	do
-#			DIR='./'$FILE'/cb0'$B'_a'$A
-#			mkdir -p $DIR
-#			./build/$EXEC -i ./Images/'carre2Degrades64b0'$B'.pgm' 						-o $DIR/'cb0'$B'_a'$A 	-t $T -a $A --lambda-1 $L1 --lambda-2 $L2 --lambda-ratio $LR --epsilon-1 $E1 --epsilon-2 $E2
-#			
-#			DIR='./'$FILE'/lb0'$B'_a'$A
-#			mkdir -p $DIR
-#			./build/$EXEC -i ./Images/'losange2DegradesDroite64b0'$B'.pgm' 		-o $DIR/'lb0'$B'_a'$A 	-t $T -a $A --lambda-1 $L1 --lambda-2 $L2 --lambda-ratio $LR --epsilon-1 $E1 --epsilon-2 $E2
-#			
-#			DIR='./'$FILE'/l30b0'$B'_a'$A
-#			mkdir -p $DIR
-#			./build/$EXEC -i ./Images/'losange2Degrades30Droite64b0'$B'.pgm' 	-o $DIR/'l30b0'$B'_a'$A -t $T -a $A --lambda-1 $L1 --lambda-2 $L2 --lambda-ratio $LR --epsilon-1 $E1 --epsilon-2 $E2
-#			
-#			DIR='./'$FILE'/cbb0'$B'_a'$A
-#			mkdir -p $DIR
-#			./build/$EXEC -i ./Images/'cercle-barre-b'$B'.pgm' 	-o $DIR/'cbb0'$B'_a'$A -t $T -a $A --lambda-1 $L1 --lambda-2 $L2 --lambda-ratio $LR --epsilon-1 $E1 --epsilon-2 $E2
-#	done
-#done
+DATA='triple-spirale'
+IM='triple-spirale-b01'
+REP=$FILE'/'$IM
+mkdir $REP
+./build/$PROG -i ./Images/$IM.pgm -d ./Images/$DATA.pgm -o $REP'/tsb1'  --alpha-1 1 --alpha-2 0.01 --alpha-ratio 1.2 --lambda-1 0.01 --lambda-2 0.0001 --lambda-ratio 1.2 --epsilon-1 1 --epsilon-2 0.25
+IM='triple-spirale-b02'
+REP=$FILE'/'$IM
+mkdir $REP
+./build/$PROG -i ./Images/$IM.pgm -d ./Images/$DATA.pgm -o $REP'/tsb2'  --alpha-1 1 --alpha-2 0.01 --alpha-ratio 1.2 --lambda-1 0.01 --lambda-2 0.0001 --lambda-ratio 1.2 --epsilon-1 1 --epsilon-2 0.25
+IM='triple-spirale-b04'
+REP=$FILE'/'$IM
+mkdir $REP
+./build/$PROG -i ./Images/$IM.pgm -d ./Images/$DATA.pgm -o $REP'/tsb4'  --alpha-1 1 --alpha-2 0.01 --alpha-ratio 1.2 --lambda-1 0.01 --lambda-2 0.0001 --lambda-ratio 1.2 --epsilon-1 1 --epsilon-2 0.25
+IM='triple-spirale-b08'
+REP=$FILE'/'$IM
+mkdir $REP
+./build/$PROG -i ./Images/$IM.pgm -d ./Images/$DATA.pgm -o $REP'/tsb8'  --alpha-1 1 --alpha-2 0.01 --alpha-ratio 1.2 --lambda-1 0.01 --lambda-2 0.0001 --lambda-ratio 1.2 --epsilon-1 1 --epsilon-2 0.25
 
 
-## lena
-
-#E1=1.0
-#E2=0.25
-
-#L1=0.05
-#L2=0.001
-#LR=1.2
-
-#T=128
-
-#BRUIT=("2")
-#ALPHA_B01="0.1 		0.2 	0.5"
-#ALPHA_B02="0.4"	#"0.05 	0.1 	0.25"
-#ALPHA_B=("$ALPHA_B02")
-
-#for I in $(seq 0 1 3)
-#do
-#	B=${BRUIT[$I]}
-#	ALPHA=${ALPHA_B[$I]}
-#	for A in $ALPHA
-#	do
-#			DIR='./'$FILE'/nb0'$B'_a'$A
-#			mkdir -p $DIR
-#			./build/$EXEC -i ./Images/'lena128b0'$B'.pgm' -o $DIR/'nb0'$B'_a'$A -t $T -a $A --lambda-1 $L1 --lambda-2 $L2 --lambda-ratio $LR --epsilon-1 $E1 --epsilon-2 $E2
-#	done
-#done
+DATA='barbara-cropped'
+IM='barbara-cropped-b01'
+REP=$FILE'/'$IM
+mkdir $REP
+./build/$PROG -i ./Images/$IM.pgm -d ./Images/$DATA.pgm -o $REP'/bb1'  --alpha-1 1 --alpha-2 0.01 --alpha-ratio 1.2 --lambda-1 0.01 --lambda-2 0.0001 --lambda-ratio 1.2 --epsilon-1 1 --epsilon-2 0.25
+IM='barbara-cropped-b02'
+REP=$FILE'/'$IM
+mkdir $REP
+./build/$PROG -i ./Images/$IM.pgm -d ./Images/$DATA.pgm -o $REP'/bb2'  --alpha-1 1 --alpha-2 0.01 --alpha-ratio 1.2 --lambda-1 0.01 --lambda-2 0.0001 --lambda-ratio 1.2 --epsilon-1 1 --epsilon-2 0.25
 
 
-## peppers
-
-#E1=1.0
-#E2=0.25
-
-#L1=0.05
-#L2=0.001
-#LR=1.2
-
-#T=256
-
-#BRUIT=("05" "2" "4")
-#ALPHA_B005="0.2 		0.4 	"
-#ALPHA_B02="	0.05 		0.1 	0.25"
-#ALPHA_B04="	0.025 	0.05	0.12"
-#ALPHA_B=("$ALPHA_B005" "$ALPHA_B02" "$ALPHA_B04")
-
-#for I in $(seq 0 1 3)
-#do
-#	B=${BRUIT[$I]}
-#	ALPHA=${ALPHA_B[$I]}
-#	for A in $ALPHA
-#	do
-#			DIR='./'$FILE'/pb0'$B'_a'$A
-#			mkdir -p $DIR
-#			./build/$EXEC -i ./Images/'peppers-256-b0'$B'.pgm' -o $DIR/'pb0'$B'_a'$A -t $T -a $A --lambda-1 $L1 --lambda-2 $L2 --lambda-ratio $LR --epsilon-1 $E1 --epsilon-2 $E2
-#	done
-#done
+DATA='mandrill-240'
+IM='mandrill-240-b01'
+REP=$FILE'/'$IM
+mkdir $REP
+./build/$PROG -i ./Images/$IM.ppm -d ./Images/$DATA.ppm -o $REP'/mb1'  --alpha-1 1 --alpha-2 0.01 --alpha-ratio 1.2 --lambda-1 0.01 --lambda-2 0.0001 --lambda-ratio 1.2 --epsilon-1 1 --epsilon-2 0.25
+IM='mandrill-240-b02'
+REP=$FILE'/'$IM
+mkdir $REP
+./build/$PROG -i ./Images/$IM.ppm -d ./Images/$DATA.ppm -o $REP'/mb2'  --alpha-1 1 --alpha-2 0.01 --alpha-ratio 1.2 --lambda-1 0.01 --lambda-2 0.0001 --lambda-ratio 1.2 --epsilon-1 1 --epsilon-2 0.25
+IM='mandrill-240-b04'
+REP=$FILE'/'$IM
+mkdir $REP
+./build/$PROG -i ./Images/$IM.ppm -d ./Images/$DATA.ppm -o $REP'/mb4'  --alpha-1 1 --alpha-2 0.01 --alpha-ratio 1.2 --lambda-1 0.01 --lambda-2 0.0001 --lambda-ratio 1.2 --epsilon-1 1 --epsilon-2 0.25
 
 
 
-# triple-spirale
-
-E1=1.0
-E2=0.25
-
-L1=0.05
-L2=0.0005
-LR=1.2
-
-T=64
-
-BRUIT=("1")
-ALPHA_B01="0.3" #"0.1 		0.2		0.5 "
-ALPHA_B02="0.15 0.2 0.3 0.4"	#"0.05 	0.1 	0.25"
-ALPHA_B04="0.025 	0.05	0.12"
-ALPHA_B08="0.012	0.25	0.06"
-ALPHA_B=("$ALPHA_B01")
-
-for I in $(seq 0 1 0)
-do
-	B=${BRUIT[$I]}
-	ALPHA=${ALPHA_B[$I]}
-	for A in $ALPHA
-	do
-			DIR='./'$FILE'/tsb0'$B'_a'$A
-			mkdir -p $DIR
-			./build/$EXEC -i ./Images/'triple-spirale-b0'$B'.pgm' 						-o $DIR/'tsb0'$B'_a'$A 	-t $T -a $A --lambda-1 $L1 --lambda-2 $L2 --lambda-ratio $LR --epsilon-1 $E1 --epsilon-2 $E2
-	done
-done
-
-
-## barbara
-
-#E1=1.0
-#E2=0.25
-
-#L1=0.05
-#L2=0.001
-#LR=1.2
-
-#T=348
-
-#BRUIT=("2")
-#ALPHA_B01="0.1 		0.2 	0.5"
-#ALPHA_B02="0.25"	#"0.05 	0.1 	0.25"
-#ALPHA_B=("$ALPHA_B02")
-
-#for I in $(seq 0 1 0)
-#do
-#	B=${BRUIT[$I]}
-#	ALPHA=${ALPHA_B[$I]}
-#	for A in $ALPHA
-#	do
-#			DIR='./'$FILE'/bb0'$B'_a'$A
-#			mkdir -p $DIR
-#			./build/$EXEC -i ./Images/'barbara-cropped-b0'$B'.pgm' -o $DIR/'bb0'$B'_a'$A -t $T -a $A --lambda-1 $L1 --lambda-2 $L2 --lambda-ratio $LR --epsilon-1 $E1 --epsilon-2 $E2
-#	done
-#done
-
+DATA='lena-370'
+IM='lena-370-b01'
+REP=$FILE'/'$IM
+mkdir $REP
+./build/$PROG -i ./Images/$IM.ppm -d ./Images/$DATA.ppm -o $REP'/lb1'  --alpha-1 1 --alpha-2 0.01 --alpha-ratio 1.2 --lambda-1 0.01 --lambda-2 0.0001 --lambda-ratio 1.2 --epsilon-1 1 --epsilon-2 0.25
+IM='lena-370-b02'
+REP=$FILE'/'$IM
+mkdir $REP
+./build/$PROG -i ./Images/$IM.ppm -d ./Images/$DATA.ppm -o $REP'/lb2'  --alpha-1 1 --alpha-2 0.01 --alpha-ratio 1.2 --lambda-1 0.01 --lambda-2 0.0001 --lambda-ratio 1.2 --epsilon-1 1 --epsilon-2 0.25
+IM='lena-370-b04'
+REP=$FILE'/'$IM
+mkdir $REP
+./build/$PROG -i ./Images/$IM.ppm -d ./Images/$DATA.ppm -o $REP'/lb4'  --alpha-1 1 --alpha-2 0.01 --alpha-ratio 1.2 --lambda-1 0.01 --lambda-2 0.0001 --lambda-ratio 1.2 --epsilon-1 1 --epsilon-2 0.25
 
